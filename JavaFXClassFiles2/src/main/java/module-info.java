735d4340b378d7d405728d1b9770b1bac007523b
nahsirk13 <63687682+nahsirk13@users.noreply.github.com>
2024-10-08 17:29:43 -0400

    Delete JavaFXClassFiles2 directory

--- JavaFXClassFiles2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafxclassfiles2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.javafxclassfiles2 to javafx.fxml;
-    exports com.example.javafxclassfiles2;
-}
